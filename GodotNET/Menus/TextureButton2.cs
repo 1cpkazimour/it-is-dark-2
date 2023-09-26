e3ad0c4227ea5d51d536f783af6c4f5fb55df598
rorokapur <kapurkrohan@gmail.com>
2023-09-26 12:49:52 -0700

    added scene changing to level selector

---
+++ GodotNET/Menus/TextureButton2.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class TextureButton2 : TextureButton
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	// Changes scene to level select when pressed
+}
